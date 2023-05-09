3713b1efbf20f3f235f07847054f2a124de798d7
Siwe09 <nontombimajenge@gmail.com>
2023-05-09 15:02:21 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(void);
+int _putchar(char c);
 int main(void);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
