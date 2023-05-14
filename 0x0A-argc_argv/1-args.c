cb4e63b947495d1971de5994ee2ac6e138c8f90d
Siwe09 <nontombimajenge@gmail.com>
2023-05-14 10:47:19 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,11 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("argv[%d] = %s\n", i, argv[i]);
-	}
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
