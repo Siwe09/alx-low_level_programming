f2b7e72768a9638e09725f3fab7b63dc96c0a4a3
Siwe09 <nontombimajenge@gmail.com>
2023-05-22 16:13:10 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @struct dog
+ * Description: Represents a dog with its attributes and behaviors.
+ *
+ * Return: Always 0 (Success)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
